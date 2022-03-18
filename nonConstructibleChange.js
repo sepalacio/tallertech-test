d6e98eb58e8b8feed4fdd359dbe0109188575a13
Sebastian Palacio <spalacio@namutek.com>
2022-03-18 11:07:03 -0500

    Add algorithms to solving nonConstructiveChange  and sortedSquaredArray challenges.

--- nonConstructibleChange.js
+++ nonConstructibleChange.js
@@ -2,3 +2,41 @@
 write a function that returns the minimum amount of change (the  minimum sum of money)
 that you  CANNOT create. The given coins can have any positive integer value and
 aren't necessarily unique (i.e., you can have multiple coins of the same value). */
+
+const isHighestMinimumChange = (minimumChange, coinValue) => (minimumChange + 1) < coinValue;
+
+const getMinimumPossibleChange = (minimumChange, coinValue) => (
+  isHighestMinimumChange(minimumChange, coinValue)
+    ? minimumChange
+    : minimumChange + coinValue
+);
+
+const sortCoinsByValue = (currentCoin, previousCoin) => currentCoin - previousCoin;
+
+const minimumChangeAmount = (coins) => {
+  const minimumChange = coins.sort(sortCoinsByValue).reduce(getMinimumPossibleChange, 0)
+
+  return minimumChange + 1;
+};
+
+const testCases = [
+  [5, 7, 1, 1, 2, 3, 22],
+  [1, 1, 1, 1, 1],
+  [1, 5, 1, 1, 1, 10, 15, 20, 100],
+]
+
+testCases.map((test, index) => {
+  console.info(`Test |${index + 1}|`)
+  console.info(`Minimum not possible change: ${minimumChangeAmount(test)}`)
+});
+
+// Solution:
+// As we have an unsorted array of positive numbers, it make sense to sort it
+// in order to accumulate the current maximum value in a variable, once you
+// iterate over every coin, it is possible to compare the current sum+1 and
+// determine if the new possible sum belongs to the minimum possible change
+// to be retrieved.
+
+// According to Big O notation, the time complexity of the solution is linear as it grows
+// depending on the amount of coins in the array.
+
--- sortedSquaredArray.js
+++ sortedSquaredArray.js
@@ -1,3 +1,53 @@
 /* Write a function that takes in a non-empty array of integers that are sorted
 in ascending order and returns a new array of the same length with the squares
 of the original integers also sorted in ascending order. */
+
+const reverseItems = (current, previous) => current - previous;
+
+const getSquare = (number) => number ** 2;
+
+const squareArrayItems = (array) => array
+  .map(getSquare)
+  .sort(reverseItems);
+
+const testCases = [
+  [1, 2, 3, 5, 6, 8, 9],
+  [-2, -1],
+  [-10, -5, 0, 5, 10],
+]
+
+testCases.map((test, index) => {
+  console.info(`Test |${index + 1}|`)
+  console.info(`Squared sorted array: ${squareArrayItems(test)}`)
+});
+
+
+// Performance analysis:
+
+// The solution above has a linear time complexity since time grows as the input is bigger.
+// One possible way to improve performance could be implementing a memoization technique
+// in which we store previously computed squares, so they do not need to be recalculated then again,
+// this solution improves performance but consumes more memory over time.
+
+// As the sorting method is an expensive operation with big arrays, we can also improve performance
+// on it by storing the items in a Binary Search Tree, then performing in-order traversal to have
+// the items sorted, with this approach we can get a logarithmical time complexity just on the sorting operation.
+
+// Down below are some results on the faster way to get a Pow in some javascript methods,
+// the one that throws the best result in Chrome devtools  is: number ** 2 (pow)
+// with a time response of 0.005859375 ms for the pow of 20.000
+
+/* console.time('Math.pow');
+  Math.pow(20000, 2)
+console.timeEnd('Math.pow');
+0.008056640625 ms */
+
+/* console.time('Number multiplied by itself');
+20000 * 20000
+console.timeEnd('Number multiplied by itself');
+0.014892578125 ms */
+
+/* console.time('Exponential expression');
+20000 ** 2
+console.timeEnd('Exponential expression');
+0.005859375 ms */
