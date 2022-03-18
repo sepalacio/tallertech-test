87ab03be3d3a4e020366e63908ab442576eb7a8c
Sebastian Palacio <spalacio@namutek.com>
2022-03-18 08:49:48 -0500

    initial commit

---
+++ nonConstructibleChange.js
@@ -0,0 +1,4 @@
+/* Given an array of positive integers representing the values of coins in your possession,
+write a function that returns the minimum amount of change (the  minimum sum of money)
+that you  CANNOT create. The given coins can have any positive integer value and
+aren't necessarily unique (i.e., you can have multiple coins of the same value). */
